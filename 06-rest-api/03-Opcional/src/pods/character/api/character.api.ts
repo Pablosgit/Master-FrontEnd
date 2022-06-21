c4d3cf0ba8bf04b01266f96c18de8b9e7fec9614
P@Git147b <pablosg.disegno@gmail.com>
2022-06-22 00:55:53 +0200

    change page character

--- 06-rest-api/03-Opcional/src/pods/character/api/character.api.ts
+++ 06-rest-api/03-Opcional/src/pods/character/api/character.api.ts
@@ -1,14 +1,30 @@
-import Axios from 'axios';
+
 import { Character } from './character.api-model';
+import { gql } from "graphql-request"
+import { graphQLClient } from 'core/api/graphql.client';
 
 
-const getUrl = (id:string) => {
-  return "https://rickandmortyapi.com/api/character/" + id;
-}
 
-export const getCharacter = async (id: string): Promise<Character> => {
-  return Axios.get<Character>(getUrl(id)).then((response) =>{
-    return response.data;
-  });
+export const getCharacter = async (id:number): Promise<Character> => {
+  console.log(id)
+  const  query = gql `
+    query {
+      character(id:${id}) {
+          name
+          id
+          status
+          species
+          type
+          gender
+          image
+        }
+    }
+  `;
+
+  const { character } = await graphQLClient.request(query)
+
+return character;
+
 };
+
 
